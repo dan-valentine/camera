7eed956f3939fa1f75de50fda0ddeb5cd19e9821
Daniel Valentine <dvalentine@Virtusa.com>
2018-08-21 12:59:37 -0400

    a test for 2 cameras

--- src/App.js
+++ src/App.js
@@ -1,74 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import Webcam from "react-webcam";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       front: false,
       back: false,
       chosen: null
-    }
+    };
   }
 
   cameraFront = () => {
-    this.setState({ front: true, chosen: true })
-  }
+    this.setState({ front: true, chosen: true });
+  };
 
   cameraBack = () => {
-    this.setState({ back: true, chosen: true })
-  }
+    this.setState({ back: true, chosen: true });
+  };
 
   render() {
-    let videoConstraints
-    let back = this.state.back
-    let front = this.state.front
-    let chosen = this.state.chosen
-    navigator.mediaDevices.enumerateDevices().then(devices => console.log(devices))
-    
-    if(front) {videoConstraints = {
-      facingMode: { exact: "user" }
-    }};
-    if(back) {
+    let videoConstraints;
+    let back = this.state.back;
+    let front = this.state.front;
+    let chosen = this.state.chosen;
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(devices => console.log(devices));
+
+    if (front) {
       videoConstraints = {
-      facingMode: { exact: "environment" }
-    }};
+        facingMode: { exact: "user" }
+      };
+    }
+    if (back) {
+      videoConstraints = {
+        facingMode: { exact: "environment" }
+      };
+    }
 
+    console.log("back: " + this.state.back);
+    console.log("front: " + this.state.front);
+    console.log("chosen: " + this.state.chosen);
+    console.log(videoConstraints);
 
-    console.log('back: ' + this.state.back)
-    console.log('front: ' + this.state.front)
-    console.log('chosen: ' + this.state.chosen)
-    console.log(videoConstraints)
-    
-    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          We Testing Things
-        </p>
+        <p className="App-intro">We Testing Things</p>
         <div>
           <button onClick={this.cameraFront}> Front</button>
           <button onClick={this.cameraBack}> Back</button>
         </div>
-        
-        { this.state.chosen ? <Webcam videoConstraints={videoConstraints} />
+        <Webcam
+          videoConstraints={{
+            facingMode: { exact: "environment" }
+          }}
+        />
+        <Webcam
+          videoConstraints={{
+            facingMode: "user"
+          }}
+        />
+
+        {/* { this.state.chosen ? <Webcam videoConstraints={videoConstraints} />
         : null
         }
         <br />
         <br />
         <br />
-        <br />
+        <br /> */}
         {/* <Webcam videoConstraints={videoConstraints} />/ */}
       </div>
     );
   }
 }
-
 
 export default App;
