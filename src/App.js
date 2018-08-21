844b288044ace06ecd61a0c7ca8583fc19ba75c4
JCamp-12 <JChandlerCampbell@gmail.com>
2018-08-20 23:46:54 -0400

    gonna get this

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
 
 class App extends Component {
   render() {
+    navigator.mediaDevices.enumerateDevices().then(devices => console.log(devices))
+    const videoConstraints = {
+      facingMode: { exact: "environment" }
+    };
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <p className="App-intro">
+          
+        </p> */}
+        <Webcam videoConstraints={videoConstraints} />
+        
+        {/* <Webcam /> */}
       </div>
     );
   }
